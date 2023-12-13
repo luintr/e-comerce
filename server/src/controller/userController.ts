a611f88cf0b657f64cbea308cb312314fb380966
luin <1103@generative.xyz>
2023-12-13 16:31:18 +0700

    feat: register endpoint

--- server/src/controller/userController.ts
+++ server/src/controller/userController.ts
@@ -1,33 +1,18 @@
 import asyncHandler from "../middleware/asyncHandler";
 import User from "../model/userModel";
-import jwt, { Secret } from "jsonwebtoken";
 import { Request, Response } from "express";
+import generateToken from "../utils/generateToken";
 // @desc      Auth user & get Token
 // @route     POST /api/users/login
 // @access    Public
-export const authUser = asyncHandler(async (req: any, res: any) => {
+export const authUser = asyncHandler(async (req: Request, res: Response) => {
   const { email, password } = req.body
 
   const user = await User.findOne({ email })
 
   if (user && (await user.matchPassword(password))) {
-    const jwtSecret: Secret | undefined = process.env.JWT_SECRET;
 
-    if (!jwtSecret) {
-      res.status(500).send('Internal Server Error: JWT_SECRET is not defined');
-      return;
-    }
-
-    const token = jwt.sign({ userId: user._id }, jwtSecret, {
-      expiresIn: '30d'
-    })
-
-    res.cookie('jwt', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== 'development',
-      sameSite: 'strict',
-      maxAge: 30 * 24 * 60 * 60 * 1000
-    });
+    generateToken(res, user._id)
 
     res.send({
       _id: user._id,
@@ -44,8 +29,33 @@ export const authUser = asyncHandler(async (req: any, res: any) => {
 // @desc      Register User
 // @route     POST /api/users
 // @access    Public
-export const registerUser = asyncHandler(async (req: any, res: any) => {
-  res.send('register user')
+export const registerUser = asyncHandler(async (req: Request, res: Response) => {
+  const { name, email, password } = req.body
+
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists');
+  }
+
+  const user = await User.create({
+    name, email, password
+  })
+
+  if (user) {
+    generateToken(res, user._id)
+
+    res.status(201).send({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user')
+  }
 })
 
 // @desc      Logout User / clear cookie
@@ -53,47 +63,47 @@ export const registerUser = asyncHandler(async (req: any, res: any) => {
 // @access    Private
 export const logoutUser = asyncHandler(async (req: Request, res: Response) => {
   res.clearCookie('jwt')
-  res.send('logout user')
+  res.status(200).send({ message: 'Logout successfully' })
 })
 
 // @desc      Get User Profile
 // @route     GET /api/users/profile
 // @access    Public
-export const getUserProfile = asyncHandler(async (req: any, res: any) => {
+export const getUserProfile = asyncHandler(async (req: Request, res: Response) => {
   res.send('get user profile')
 })
 
 // @desc      Update User Profile
 // @route     PUT /api/users/profile
 // @access    Private
-export const updateUserProfile = asyncHandler(async (req: any, res: any) => {
+export const updateUserProfile = asyncHandler(async (req: Request, res: Response) => {
   res.send('update user profile')
 })
 
 // @desc      Get Users
 // @route     GET /api/users
 // @access    Private/Admin
-export const getUsers = asyncHandler(async (req: any, res: any) => {
+export const getUsers = asyncHandler(async (req: Request, res: Response) => {
   res.send('get users')
 })
 
 // @desc      Delete Users
 // @route     DELETE /api/users/:id
 // @access    Private/Admin
-export const deleteUser = asyncHandler(async (req: any, res: any) => {
+export const deleteUser = asyncHandler(async (req: Request, res: Response) => {
   res.send('delete user')
 })
 
 // @desc      Get Users by ID
 // @route     GET /api/users/:id
 // @access    Private/Admin
-export const getUserById = asyncHandler(async (req: any, res: any) => {
+export const getUserById = asyncHandler(async (req: Request, res: Response) => {
   res.send('get user id')
 })
 
 // @desc      Update User
 // @route     POST /api/users/:id
 // @access    Private/Admin
-export const updateUser = asyncHandler(async (req: any, res: any) => {
+export const updateUser = asyncHandler(async (req: Request, res: Response) => {
   res.send('update user')
 })
---
+++ server/src/utils/generateToken.ts
@@ -0,0 +1,24 @@
+import jwt, { Secret } from 'jsonwebtoken'
+import { Response } from 'express'
+
+const generateToken = (res: Response, userId: string) => {
+  const jwtSecret: Secret | undefined = process.env.JWT_SECRET;
+
+  if (!jwtSecret) {
+    res.status(500).send('Internal Server Error: JWT_SECRET is not defined');
+    return;
+  }
+
+  const token = jwt.sign({ userId }, jwtSecret, {
+    expiresIn: '30d'
+  })
+
+  res.cookie('jwt', token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== 'development',
+    sameSite: 'strict',
+    maxAge: 30 * 24 * 60 * 60 * 1000
+  });
+}
+
+export default generateToken
