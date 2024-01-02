f45c59e3b89e62d9eaa2a90a740e773f7f79d338
luintr <hainam.huflit@gmail.com>
2024-01-02 22:44:46 +0700

    feat: orders list

--- client/src/modules/Profile/index.tsx
+++ client/src/modules/Profile/index.tsx
@@ -3,11 +3,12 @@
 import React, { useEffect, useState } from 'react';
 import s from './styles.module.scss';
 import { useDispatch, useSelector } from 'react-redux';
-import { Button, Form, Input, message } from 'antd';
+import { Button, Form, Input, Table, Tag, message } from 'antd';
 import { profile } from '@/api/userAPI';
 import { setCredentials } from '@/store/slices/authSlice';
 import { useRouter } from 'next/navigation';
 import { getMyOrders } from '@/api/orderAPI';
+import Link from 'next/link';
 const ProfileModule = () => {
   const [messageApi, contextHolder] = message.useMessage();
   const [orders, setOrders] = useState<any>(null);
@@ -22,7 +23,7 @@ const ProfileModule = () => {
       setOrders(res);
     });
   }, []);
-  console.log(orders);
+
   const onFinish = async (value: any) => {
     try {
       const res = await profile({
@@ -47,6 +48,64 @@ const ProfileModule = () => {
     }
   };
 
+  const columns = [
+    {
+      title: 'ID',
+      dataIndex: '_id',
+      key: '_id',
+      render: (text: string) => {
+        const shortenedString = text.substring(0, 15);
+        return <Link href={`/order/${text}`}>{shortenedString}...</Link>;
+      },
+      width: '40%',
+    },
+    {
+      title: 'Date',
+      dataIndex: 'createdAt',
+      key: 'createdAt',
+      render: (text: string) => {
+        const dateObject = new Date(text);
+        return `${dateObject.getFullYear()}-${
+          dateObject.getMonth() + 1
+        }-${dateObject.getDate()}`;
+      },
+      width: '30%',
+    },
+    {
+      title: 'Total',
+      dataIndex: 'totalPrice',
+      key: 'totalPrice',
+      render: (text: string) => `$${text}`,
+      width: '30%',
+    },
+    {
+      title: 'Paid',
+      dataIndex: 'isPaid',
+      key: 'isPaid',
+      render: (isPaid: boolean) => {
+        if (isPaid) {
+          return <Tag color={'green'}>{isPaid}</Tag>;
+        } else {
+          return <Tag color={'volcano'}>Not Paid</Tag>;
+        }
+      },
+      width: '30%',
+    },
+    {
+      title: 'Delivered',
+      dataIndex: 'isDelivered',
+      key: 'isDelivered',
+      render: (isDelivered: boolean) => {
+        if (isDelivered) {
+          return <Tag color={'green'}>{isDelivered}</Tag>;
+        } else {
+          return <Tag color={'volcano'}>Not Deliveried</Tag>;
+        }
+      },
+      width: '30%',
+    },
+  ];
+
   return (
     <div className={`${s.profile} container grid grid-cols-12`}>
       {contextHolder}
@@ -60,7 +119,7 @@ const ProfileModule = () => {
         }}
         onFinish={onFinish}
         autoComplete="on"
-        className={`col-span-6`}
+        className={`col-span-5`}
       >
         <Form.Item
           name="email"
@@ -135,8 +194,9 @@ const ProfileModule = () => {
           </Button>
         </Form.Item>
       </Form>
-      <div className={`col-span-6`}>
-        <h2>Orders</h2>
+      <div className={`col-span-7`}>
+        <h2>Your Orders</h2>
+        <Table columns={columns} dataSource={orders} />
       </div>
     </div>
   );
